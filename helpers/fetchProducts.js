991951045172dae534d033c27d876cb6baaba800
Thawane <thawaneapoliveira@gmail.com>
2022-09-12 16:28:49 -0300

    Adiciona requisito 1 funcionando

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,15 +1,21 @@
-const fetchProducts = async () => {
-  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+// const fetchProducts = async (merch) => {
+
+const fetchProducts = async (goods) => {
+  // if (!goods) {
+  //   throw new Error('You must provide an url');
+  // }
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${goods}`;
+  // const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
   const products = await fetch(url)
   .then((response) => response.json())
-  .then((info) => info.results)
-  .catch((error) => console.log({ error }));
+  .then((info) => info)
+  .catch((error) => error);
 
   return products;
 };
 
-console.log(fetchProducts());
+// recebi ajuda do Tiago Quadros na mentoria
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,21 +1,37 @@
 require('../mocks/fetchSimulator');
-const { expect } = require('chai');
+// const { expect } = require('chai');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
+const { expect } = require('@jest/globals');
 
-describe('1 - Teste a função fetchProducts', async () => {
+describe('1 - Teste a função fetchProducts', () => {
 
-  const response = await fetchProducts('computador');
-  console.log(response);
-  
-  expect(fetchProducts).toBe('function');
+  // const response = fetchProducts('computador');
+  // console.log(response);
 
-  expect(fetch).toHaveBeenCalled(fetchProducts('computador'));
+  it('Teste se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  }); 
 
-  expect(fetchProducts('computador').toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador'))
+  it('Teste se a fetch é chamada com a função tendo computador como parâmetro', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  }); 
 
-  expect(fetchProducts('computador')).lessThanOrEqual(computadorSearch);
+  it('Teste se o endpoint https://api.mercadolibre.com/sites/MLB/search?q=computador é utilizado ao executar a função com computador como parâmetro', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
 
-  expect(fetchProducts()).toEqual(new Error('You must provide an url'));
+  it('Teste se o retorno da função com parâmetro computador tem estrutura de dados igual ao objeto computadorSearch', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+
+  it('Teste se a função sem parâmetro retorna You must provide an url', async () => {
+    const empty = await fetchProducts();
+    
+    expect(empty).toEqual(new Error('You must provide an url'));
+  });
   // fail('Teste vazio');
 });
