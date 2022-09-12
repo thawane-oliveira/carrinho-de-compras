6f18f4fe9f63c0bd3ba34ae413ca03e0e81282bc
Thawane <thawaneapoliveira@gmail.com>
2022-09-12 16:37:57 -0300

    Adiciona ajuste

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,18 +1,14 @@
 // const fetchProducts = async (merch) => {
 
 const fetchProducts = async (goods) => {
-  // if (!goods) {
-  //   throw new Error('You must provide an url');
-  // }
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${goods}`;
-  // const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
 
-  const products = await fetch(url)
-  .then((response) => response.json())
-  .then((info) => info)
-  .catch((error) => error);
+  return fetch(url)
+    .then((response) => response.json())
+    .then((info) => info)
+    .catch((error) => error);
 
-  return products;
+  // return products;
 };
 
 // recebi ajuda do Tiago Quadros na mentoria
