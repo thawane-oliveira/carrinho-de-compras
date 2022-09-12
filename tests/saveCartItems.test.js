9447411c05cab6564730c9118e5900f29149c346
Thawane <thawaneapoliveira@gmail.com>
2022-09-12 17:23:07 -0300

    Adiciona requisito 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -1,9 +1,18 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
+const { expect } = require('@jest/globals');
 
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se com cartItem como parâmetro, o localStorage.setItem é chamado', () => {
+    saveCartItems('jotaro');
+    expect(localStorage.setItem).toBeCalled();
+  }); 
+
+  it('Teste se com cartItem como parâmetro, o localStorage.setItem é chamado com os parâmetros: o primeiro a chave cartItems e o segundo o argumento passado na função saveCartItems', () => {
+    saveCartItems('kakyoin');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', 'kakyoin');
+  });
+  // fail('Teste vazio');
 });
