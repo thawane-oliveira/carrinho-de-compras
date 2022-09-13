fc6ebb5f2e69b6f39be2950ef34315985064e2a6
Thawane <thawaneapoliveira@gmail.com>
2022-09-13 00:18:12 -0300

    Adiciona ajuste de lint

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const createProductItemElement = ({ id, title, thumbnail }) => {
  * @param {Element} product - Elemento do produto.
  * @returns {string} ID do produto.
  */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
+// const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
 
 /**
  * Função responsável por criar e retornar um item do carrinho.
